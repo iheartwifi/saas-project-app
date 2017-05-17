677563888ca819ee476362a89274879bb9b4448a
Wan Lan He <wanlanhe@gmail.com>
2017-05-17 03:31:31 +0000

    add admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516210657) do
+ActiveRecord::Schema.define(version: 20170517024446) do
 
   create_table "artifacts", force: :cascade do |t|
     t.string   "name"
@@ -38,12 +38,12 @@ ActiveRecord::Schema.define(version: 20170516210657) do
   create_table "payments", force: :cascade do |t|
     t.string   "email"
     t.string   "token"
-    t.integer  "tenant_id_id"
+    t.integer  "tenant_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
 
-  add_index "payments", ["tenant_id_id"], name: "index_payments_on_tenant_id_id"
+  add_index "payments", ["tenant_id"], name: "index_payments_on_tenant_id"
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20170516210657) do
     t.integer  "tenant_id"
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
+    t.boolean  "is_admin",                     default: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
